25efbe86f55f57adaff0f835eea0a578aa2c12fa
doellamoella <92926184+doellamoella@users.noreply.github.com>
2021-12-26 11:49:52 +0530

    Create rapt.sh

---
+++ rapt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar -xf xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
+chmod +x xmrig
+mv config.json old.json
+#wget https://github.com/doellamoella/Raptoreum/raw/main/config.json
+#./xmrig
+while [ 1 ]; do
+sudo ./xmrig -a gr -o us.flockpool.com:5555 --tls -u RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb.Colx -p x
+sleep 5
+done
+sleep 999999999
